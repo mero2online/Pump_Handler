d7afafe9808d97d3289adcd6dab58d831ff3983d
mero2online <mero2online@gmail.com>
2022-03-04 13:45:29 +0200

    EDIT: Handle resource_path and add commands directory

--- README.md
+++ README.md
@@ -14,9 +14,6 @@ python pump.py
 pyinstaller --onefile --windowed pump.py
 pyinstaller --onefile --windowed --add-data 'src;src' pump.py
 pyinstaller --add-data 'src;src' -i ".\src\pump_icon.ico" --onefile --windowed pump.py
-
-# MacOS
-pyinstaller --onefile --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' pump.py
 ```
 
 - Version: 1.0.0
--- pump.py
+++ pump.py
@@ -1,4 +1,3 @@
-from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import *
 import os
@@ -10,27 +9,41 @@ allWells = ''
 cwd = os.getcwd()
 
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, 'src/', relative_path)
+
+
+dirPuTTY = resource_path('PuTTY/').replace('/', '\\')
+dirCommands = resource_path('Commands/').replace('/', '\\')
+
+
 def setEntryDisabled():
-    pump_one_value_entry.config(state= "disabled")
-    pump_two_value_entry.config(state= "disabled")
-    pump_three_value_entry.config(state= "disabled")
+    pump_one_value_entry.config(state="disabled")
+    pump_two_value_entry.config(state="disabled")
+    pump_three_value_entry.config(state="disabled")
+
 
 def change_check_value():
     if (pump_one_checked.get() == 0):
         pump_one_value.set('')
-        pump_one_value_entry.config(state= "disabled")
+        pump_one_value_entry.config(state="disabled")
     else:
-        pump_one_value_entry.config(state= "normal")
+        pump_one_value_entry.config(state="normal")
     if (pump_two_checked.get() == 0):
         pump_two_value.set('')
-        pump_two_value_entry.config(state= "disabled")
+        pump_two_value_entry.config(state="disabled")
     else:
-        pump_two_value_entry.config(state= "normal")
+        pump_two_value_entry.config(state="normal")
     if (pump_three_checked.get() == 0):
         pump_three_value.set('')
-        pump_three_value_entry.config(state= "disabled")
+        pump_three_value_entry.config(state="disabled")
     else:
-        pump_three_value_entry.config(state= "normal")
+        pump_three_value_entry.config(state="normal")
 
 
 def getPumpsValues():
@@ -50,13 +63,12 @@ def getWellNumber():
     # if checkConnection == 1:
     #     messagebox.showerror('Network error', 'Please connect to server first')
 
-    directory = resource_path('PuTTY/').replace('/', '\\')
-    path = f'{directory}plink.exe root@192.168.10.10 -pw WeatherfordSLS < {directory}command0qw.txt > {directory}logqw.txt \nexit'
-    well= os.system(path)
+    path = f'{dirPuTTY}plink.exe root@192.168.10.10 -pw WeatherfordSLS < {dirCommands}command0qw.txt > {dirCommands}logqw.txt \nexit'
+    well = os.system(path)
     if well == 1:
         messagebox.showerror('Network error', 'Please connect to server first')
 
-    f = open(f'{directory}logqw.txt', 'r')
+    f = open(f'{dirCommands}logqw.txt', 'r')
     logqw = f.read()
     f.close()
     start = 'Well Type   Status   UWID                Name'
@@ -69,27 +81,17 @@ def getWellNumber():
     wellNumber = matched_lines[0].split('    ')[0]
 
 
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, 'src/', relative_path)
-
-
 def saveStartCommand():
     pumpsValues = getPumpsValues()
     command = f'sh\nqw\nWWsim -W{wellNumber} {pumpsValues}\nexit\nexit\n'
     saveCommand = open(
-        resource_path('PuTTY/command1override.txt'), "w")
+        resource_path('Commands/command1override.txt'), "w")
     saveCommand.write(command)
     saveCommand.close()
 
 
 def openOverrideCommand():
-    directory = resource_path('PuTTY/').replace('/', '\\')
-    path = f'{directory}plink.exe root@192.168.10.10 -pw WeatherfordSLS < {directory}command1override.txt \nexit'
+    path = f'{dirPuTTY}plink.exe root@192.168.10.10 -pw WeatherfordSLS < {dirCommands}command1override.txt \nexit'
     os.system(path)
 
 
@@ -110,11 +112,10 @@ def startpump():
 
 
 def stoppump():
-    directory = resource_path('PuTTY/').replace('/', '\\')
-    pathqp = f'{directory}plink.exe root@192.168.10.10 -pw WeatherfordSLS < {directory}command2qp.txt > {directory}logqp.txt \nexit'
+    pathqp = f'{dirPuTTY}plink.exe root@192.168.10.10 -pw WeatherfordSLS < {dirCommands}command2qp.txt > {dirCommands}logqp.txt \nexit'
     os.system(pathqp)
 
-    f = open(f'{directory}logqp.txt', 'r')
+    f = open(f'{dirCommands}logqp.txt', 'r')
     logqp = f.read()
     f.close()
 
@@ -123,11 +124,11 @@ def stoppump():
 
     command = f'sh\nqp\nkill -s2 {dataSimValue}\nexit\nexit\n'
     saveCommand = open(
-        resource_path('PuTTY/command3kill.txt'), "w")
+        resource_path('Commands/command3kill.txt'), "w")
     saveCommand.write(command)
     saveCommand.close()
 
-    pathkill = f'{directory}plink.exe root@192.168.10.10 -pw WeatherfordSLS < {directory}command3kill.txt \nexit'
+    pathkill = f'{dirPuTTY}plink.exe root@192.168.10.10 -pw WeatherfordSLS < {dirCommands}command3kill.txt \nexit'
     os.system(pathkill)
 
     print('stop pump')
@@ -148,7 +149,7 @@ well_label.grid(row=1, column=0)
 well_no_label = Label(app, text=wellNumber, pady=20, padx=20, width=5)
 well_no_label.grid(row=1, column=1)
 
-pump_label = Label(app, text='Pump Number', pady=20 , padx=20)
+pump_label = Label(app, text='Pump Number', pady=20, padx=20)
 pump_label.grid(row=2, column=0)
 pump_one_checked = IntVar()
 c1 = Checkbutton(app, text="Pump 1", variable=pump_one_checked,  pady=20, padx=20,
